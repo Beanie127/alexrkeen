0ac6ee0a465e83f9a0761f1a9cfc0086777a4d5a
beanie127 <beanie127@gmail.com>
2023-10-29 09:36:37 +0000

    fork BIP app to a separate domain

--- public/bip/menu.js
+++
@@ -1,7 +0,0 @@
-const toggleNav = document.querySelector("#toggle-nav");
-const sidebar = document.querySelector("#sidebar");
-
-toggleNav.addEventListener("click", (e) => {
-  e.preventDefault;
-  sidebar.toggleAttribute("data-visible");
-});
