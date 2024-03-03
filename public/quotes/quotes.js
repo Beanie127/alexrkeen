e29891bafc55010922115fd4c7de8025ee6a0cb5
Beanie127 <beanie127@gmail.com>
2024-03-03 20:24:23 +0000

    Load animation only plays once per session. Also added Heinlein quote

--- public/quotes/quotes.js
+++ public/quotes/quotes.js
@@ -42,6 +42,10 @@ window.addEventListener("load", () => {
 
 const quoteData = [
   {
+    author: "Robert Heinlein",
+    text: "A human being should be able to change a diaper, plan an invasion, butcher a hog, conn a ship, design a building, write a sonnet, balance accounts, build a wall, set a bone, comfort the dying, take orders, give orders, cooperate, act alone, solve equations, analyze a new problem, pitch manure, program a computer, cook a tasty meal, fight efficiently, die gallantly. Specialization is for insects.",
+  },
+  {
     author: "Harry Ruby",
     text: "The best bridge between despair and hope is a good night's sleep.",
   },
