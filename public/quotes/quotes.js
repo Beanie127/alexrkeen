ce10cbde7b420d082029b0e98a89fc7d1fbf6b39
Beanie127 <beanie127@gmail.com>
2024-03-08 12:39:43 +0000

    added Linus Pauling quote

--- public/quotes/quotes.js
+++ public/quotes/quotes.js
@@ -42,6 +42,10 @@ window.addEventListener("load", () => {
 
 const quoteData = [
   {
+    author: "Linus Pauling",
+    text: "The best way to have a good idea is to have a lot of ideas.",
+  },
+  {
     author: "Robert Heinlein",
     text: "A human being should be able to change a diaper, plan an invasion, butcher a hog, conn a ship, design a building, write a sonnet, balance accounts, build a wall, set a bone, comfort the dying, take orders, give orders, cooperate, act alone, solve equations, analyze a new problem, pitch manure, program a computer, cook a tasty meal, fight efficiently, die gallantly. Specialization is for insects.",
   },
