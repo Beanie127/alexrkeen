53dc7e21d1312bb6e78b9f2b850ad5f7fe66a3b9
Beanie127 <beanie127@gmail.com>
2024-03-16 00:35:46 +0000

    added slightly jank card flip animation

--- public/experiments/card-games/main.js
+++ public/experiments/card-games/main.js
@@ -6,13 +6,22 @@ const displayCard = document.querySelector("#display-card");
 
 const deck = new TarotDeck();
 
+let cardsPlayed = 0;
+
 btnDrawCard.addEventListener("click", (e) => {
+  cardsPlayed++;
   displayCard.innerHTML += `
-  <div class="playing-card">${deck.draw().name}</div>
+  <div class="playing-card place-card" data-index="${cardsPlayed}">${
+    deck.draw().name
+  }</div>
   `;
+  setTimeout(() => {
+    document.querySelector(".place-card").classList.remove("place-card");
+  }, 601);
 });
 
 btnShuffle.addEventListener("click", (e) => {
   displayCard.innerHTML = ``;
   deck.shuffle();
+  cardsPlayed = 0;
 });
