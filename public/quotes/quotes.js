479046128915d3f5e6497322c60eb2b47cce1ea6
Beanie127 <beanie127@gmail.com>
2024-07-23 16:34:18 +0100

    added Ursula K Le Guin

--- public/quotes/quotes.js
+++ public/quotes/quotes.js
@@ -42,6 +42,10 @@ window.addEventListener("load", () => {
 
 const quoteData = [
   {
+    author: "Ursula K. Le Guin",
+    text: "A child free from the guilt of ownership and the burden of economic competition will grow up with the will to do what needs doing and the capacity for joy in doing it. It is useless work that darkens the heart. The delight of the nursing mother, of the scholar, of the successful hunter, of the good cook, of the skilful maker, of anyone doing needed work and doing it well — this durable joy is perhaps the deepest source of human affection and of sociality as a whole.",
+  },
+  {
     author: "Denis Diderot",
     text: "Beware of the man who talks about putting things in order! Putting things in order always means getting other people under your control.",
   },
