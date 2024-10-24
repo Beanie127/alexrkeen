274f47a6fdc0d2b1ee1d02a7ecbac0c1aeb8f35d
Beanie127 <beanie127@gmail.com>
2024-10-24 15:13:26 +0100

    NEW: added capitalisation

--- public/testing/suggestion-generator/utilities.js
+++ public/testing/suggestion-generator/utilities.js
@@ -2,6 +2,10 @@ function randomNumber(cap) {
   return Math.floor(Math.random() * cap);
 }
 
+export function capitalise(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 export default function randFromArray(array) {
   const num = randomNumber(array.length);
   return array[num];
