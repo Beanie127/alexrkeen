cfa67f794217ac5260030ea8845d43eba8154385
Beanie127 <beanie127@gmail.com>
2024-10-31 21:41:52 +0000

    Added Bessie A Stanley, Socrates, Sabaa Tahir

--- public/quotes/quotes.js
+++ public/quotes/quotes.js
@@ -49,6 +49,18 @@ quoteFilter.addEventListener("keyup", (e) => {
 
 const allQuotes = [
   {
+    text: "To laugh often and much; to win the respect of intelligent people and the affection of children; to earn the appreciation of honest critics and endure the betrayal of false friends; to appreciate beauty; to find the best in others; to leave the world a bit better, whether by a healthy child, a garden patch or a redeemed social condition; to know even one life has breathed easier because you have lived. This is to have succeeded.",
+    author: "Bessie A. Stanley",
+  },
+  {
+    text: "Be kind, for everyone you meet is fighting a hard battle.",
+    author: "Socrates",
+  },
+  {
+    text: "Failure doesn't define you. It's what you do after you fail that determines whether you're a leader or a waste of perfectly good air.",
+    author: "Sabaa Tahir",
+  },
+  {
     text: "We are punished by our sins, not for them.",
     author: "Elbert Hubbard",
   },
