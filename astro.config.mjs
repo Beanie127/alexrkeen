3722576ce7756f44d66fd2ecfd1d9f6dd155cc2c
beanie127 <beanie127@gmail.com>
2025-09-06 12:24:19 +0100

    Added integration to rename 404.html to not_found.html

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import { fileURLToPath } from 'node:url';
+import { rename } from 'node:fs/promises';
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+	integrations: [
+		{
+			name: 'rename-404',
+			hooks: {
+				'astro:build:done': async (params) => {
+					const notFoundFileURL = new URL('./404.html', params.dir);
+					const newNotFoundFileURL = new URL('./not_found.html', params.dir);
+
+					await rename(
+						fileURLToPath(notFoundFileURL),
+						fileURLToPath(newNotFoundFileURL)
+					);
+				},
+			},
+		},
+	],
+});
